cec58bf9dfa382cb1691dea3ff124ec1b17bf153
habibadha <76233615+habibadha@users.noreply.github.com>
2022-07-09 20:58:11 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn, text, isOwner, groupMetadata }) => {
     if (!code) throw 'Link invalid'
     if (global.db.data.users[m.sender].joinlimit == 0) return m.reply('Maaf kamu sudah tidak bisa menggunakan free join..\nHarap hubungi *owner* kami')
     global.db.data.users[m.sender].joinlimit -= 1
-   // let id = m.chat
-   // let groupMetadata = await conn.groupMetadata(m.chat)
+    let id = m.chat
+    let groupMetadata = await conn.groupMetadata(m.chat)
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(7, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
     m.reply(`Berhasil join grup ${res} selama ${expired ? ` selama ${expired} hari` : ''}`)
-   // conn.reply(`Bot telah di undang di group: ${groupMetadata.subject}\nCode ID: ${res}`, `6285828357727@s.whatsapp.net`)
+    conn.reply(`Bot telah di undang di group: ${groupMetadata.subject}\nCode ID: ${res}`, `6285828357727@s.whatsapp.net`)
     setTimeout(() => {
     conn.reply(res, `*${conn.user.name}* adalah bot whatsapp yang di bangun menggunakan Nodejs, diundang oleh @${m.sender.split`@`[0]} trial selama\n*${msToDate(global.db.data.chats[res].expired - new Date() * 1)}*\n\nUntuk Melihat List *Menu* bot ketik *#menu*\n\nJika ingin di perpanjang expired group harap hubungi *owner* kami..`.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
     }, 1500) 
@@ -18,8 +18,8 @@ let handler = async (m, { conn, text, isOwner, groupMetadata }) => {
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
 }
-//handler.help = ['join <chat.whatsapp.com>']
-//handler.tags = ['premium']
+handler.help = ['join <chat.whatsapp.com>']
+handler.tags = ['premium']
 
 handler.command = /^join$/i
 handler.premium = true

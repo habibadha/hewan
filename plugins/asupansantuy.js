bf45f3453b47542b76868f32c1a6763222e389aa
habibadha <76233615+habibadha@users.noreply.github.com>
2022-07-09 20:55:44 +0700

    Update asupansantuy.js

--- plugins/asupansantuy.js
+++ plugins/asupansantuy.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/santuy', 'asupan.mp4', 'nih asupannya kak jangan lupa subscribe channel https://m.youtube.com/channel/UC0rPHvwr0sJtaccF8Mm1Xng', m)
+	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/santuy', 'asupan.mp4', 'nih asupannya kak jangan lupa subscribe channel https://m.youtube.com/channel/hz22yt', m)
 }
 handler.help = ['asupansantuy']
-handler.tags = ['asupan']
+handler.tags = ['premium']
 
 handler.command = /^(asupansantuy)$/i
 handler.premium = true
